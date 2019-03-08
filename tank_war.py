e3fe53b23184964821e436b23fc18989d315648d
loony-lisa <39192805+loony-lisa@users.noreply.github.com>
2019-03-08 10:36:00 +0800

    translate the commends into english
    None

--- tank_war.py
+++ tank_war.py
@@ -1,40 +1,40 @@
 # coding=utf-8
-# 坦克大战
-# last update: 2018-06-29
-# by. XYJ
-#  ____________ 玩法说明 ______________
-#      上下左右键：控制玩家坦克移动
-#         空格键：发射子弹
-#         躲避敌方坦克的子弹
-#        击中敌方坦克来获得分数
-#  -----------------------------------
+# Tank War Game
+# A very simple tank war game programed by Python and Tkinter.
+# last update: 2019-03-28 by. Yijie Xu
+#
+# How to play?
+# Use arrow keys to control the direction of the Tank
+# Use space key to fire
+# Enjoy! ;)
+
 from Tkinter import *
 import time, random
 
-# 窗口的宽和高
+# Width and height of the window
 WIN_WIDTH = 600
 WIN_HEIGHT = 600
 
-# 敌方坦克图片的宽和高
+# Width and height of enemies' tanks
 E_TANK_WIDTH = 45
 E_TANK_HEIGHT = 45
 
-# 敌方坦克的数量
+# Number of enemy tanks
 E_TANK_NUM = 10
 
-# 敌方子弹的宽和高
+# Width and height of the bullet of enemies' tank
 E_BULLET_WIDTH = 16
 E_BULLET_HEIGHT = 16
 
-# 玩家坦克图片的宽和高
+# Width and height of player's tank
 P_TANK_WIDTH = 62
 P_TANK_HEIGHT = 76
 
-# 玩家坦克的子弹的宽和高
+# Width and height of player's bullet
 P_BULLET_WIDTH = 28
 P_BULLET_HEIGHT = 28
 
-# 坦克的状态
+# status of tanks
 ACTIVE = "active"
 EXPLODE = "explode"
 USELESS = "useless"
@@ -45,30 +45,29 @@ win = Tk()
 can = Canvas(win, width=WIN_WIDTH, height=WIN_HEIGHT)
 can.pack()
 
-# 导入背景图片
+# import background pictures
 bg = PhotoImage(file="pic/bg.gif")
 can.create_image(0, 0, image=bg, anchor="nw")
 
-# 所有tank的底片都将被存在tanks_img字典里
-# 访问底片的方式 tanks_img[坦克颜色][坦克方向]
+# A dictionart to store tanks' images
 tanks_img = {}
 
-# 蓝色tank各个方向的底片
+# images of a blue tank (4 directions)
 blue_tank = {}
 for d in DIRECTIONS:
     blue_tank[d] = PhotoImage(file="pic/tank_blue_" + d + ".gif")
 
-# 黑色tank各个方向的底片
+# images of a black tank (4 directions)
 black_tank = {}
 for d in DIRECTIONS:
     black_tank[d] = PhotoImage(file="pic/tank_black_" + d + ".gif")
 
-# 红色tank各个方向的底片
+# images of a red tank (4 directions)
 red_tank = {}
 for d in DIRECTIONS:
     red_tank[d] = PhotoImage(file="pic/tank_red_" + d + ".gif")
 
-# 玩家tank的颜色为"huge"
+# images of a huge(player's) tank (4 directions)
 huge_tank = {}
 for d in DIRECTIONS:
     huge_tank[d] = PhotoImage(file="pic/tank_huge_" + d + ".gif")
@@ -78,29 +77,29 @@ tanks_img["red"] = red_tank
 tanks_img["black"] = black_tank
 tanks_img["huge"] = huge_tank
 
-# tank爆炸的图片
+# images of exploring tank
 explosion = []
 for i in range(5):
     explosion.append(PhotoImage(file="pic/explosion" + str(i + 1) + ".gif"))
 
 bullets_img = {}
 
-# 蓝色子弹的底片
+# images of the blue bullet(4 directions)
 blue_bullet = {}
 for d in DIRECTIONS:
     blue_bullet[d] = PhotoImage(file="pic/bullet_blue_" + d + ".gif")
 
-# 红色子弹的底片
+# images of the red bullet(4 directions)
 red_bullet = {}
 for d in DIRECTIONS:
     red_bullet[d] = PhotoImage(file="pic/bullet_red_" + d + ".gif")
 
-# 黑色子弹的底片
+# images of the black bullet(4 directions)
 black_bullet = {}
 for d in DIRECTIONS:
     black_bullet[d] = PhotoImage(file="pic/bullet_black_" + d + ".gif")
 
-# 玩家子弹的底片
+# images of player's bullet(4 directions)
 huge_bullet = {}
 for d in DIRECTIONS:
     huge_bullet[d] = PhotoImage(file="pic/bullet_huge_" + d + ".gif")
@@ -110,25 +109,20 @@ bullets_img["red"] = red_bullet
 bullets_img["black"] = black_bullet
 bullets_img["huge"] = huge_bullet
 
-# Tank类
+
 class Tank():
 
     def __init__(self, x, y, d, c):
-        # 颜色
+        
         self.color = c
-        # 方向
         self.dir = d
-        # 速度(每帧移动的距离)
         self.speed = 5
-        # 图形元素
         self.drawable = can.create_image(x, y,
                                          image=tanks_img[self.color][self.dir],
                                          anchor="nw")
-        # 记录Tank状态
         self.state = ACTIVE
-        # 切换爆炸状态的计数器
         self.e_count = 0
-        # 坦克底片的长和高
+ 
         if self.color == "huge":
             self.width = P_TANK_WIDTH
             self.height = P_TANK_HEIGHT
@@ -136,16 +130,16 @@ class Tank():
             self.width = E_TANK_WIDTH
             self.height = E_TANK_HEIGHT
 
-    # 更新坦克的位置和底片
+    # Update the appearance and state of the tank
     def update_pos_img(self):
         if self.state == USELESS:
             pass
 
         elif self.state == EXPLODE:
-            # 切换底片形成爆炸动画
+            # change images
             can.itemconfig(self.drawable, image=explosion[self.e_count])
             self.e_count += 1
-            # 如果已经切换至最后一张底片
+            # if it is the last one
             if self.e_count == 5:
                 self.state = USELESS
                 self.e_count = 0
@@ -153,7 +147,7 @@ class Tank():
         elif self.state == ACTIVE:
             t_pos = self.get_pos()
 
-            # 撞墙后随机更改方向
+            # When touching wall, change direction arbitarily
             if t_pos[0] < 0:
                 self.dir = random.choice(["down", "up", "right"])
             elif t_pos[1] < 0:
@@ -165,7 +159,7 @@ class Tank():
 
             can.itemconfig(self.drawable, image=tanks_img[self.color][self.dir])
 
-            # 移动坦克位置
+            # change tank's position
             if self.dir == "up":
                 can.move(self.drawable, 0, -self.speed)
             elif self.dir == "left":
@@ -175,7 +169,6 @@ class Tank():
             elif self.dir == "right":
                 can.move(self.drawable, self.speed, 0)
 
-    # 创建一颗子弹
     def create_bullet(self):
         if self.color == "huge":
             b_e = P_BULLET_WIDTH
@@ -184,7 +177,7 @@ class Tank():
             b_w = E_BULLET_WIDTH
             b_h = E_BULLET_HEIGHT
 
-        # 计算出bullet应该在哪里被创建出来
+        # To calculate the initial position of a bullet
         b_pos = self.get_pos()
         if self.dir == "up":
             b_pos = [b_pos[0] + (self.width-b_w)/2, b_pos[1]]
@@ -219,7 +212,6 @@ class Tank():
         self.dir = "right"
 
 
-# Bullet类
 class Bullet():
     def __init__(self, pos, c, d, w):
         self.color = c
@@ -254,8 +246,8 @@ class Bullet():
         return b_pos
 
 
-# 主程序开始的地方
-# 敌方的坦克和子弹列表
+# MAIN PROGRAM
+# Tank and bullet list for enemies' tanks
 enemy_tanks = []
 enemy_bullets = []
 
@@ -266,7 +258,7 @@ for i in range(E_TANK_NUM):
     c = random.choice(["red", "blue", "black"])
     enemy_tanks.append(Tank(x, y, d, c))
 
-# 玩家的坦克和子弹
+# Tank and bullet list for player's tanks
 player_tank = Tank(10, 10, "down", "huge")
 player_bullets = []
 
@@ -275,7 +267,7 @@ def shoot(event):
     player_bullets.append(player_tank.create_bullet())
 
 
-# 将按键与玩家坦克的方法进行绑定
+# Bind keys with respond functions
 can.bind_all('<Up>', player_tank.set_dir_up)
 can.bind_all('<Left>', player_tank.set_dir_left)
 can.bind_all('<Down>', player_tank.set_dir_down)
@@ -313,16 +305,16 @@ score_text = can.create_text(150, 10, anchor='nw', text='score: '+str(score),
 running = True
 
 while running:
-    # 更新玩家坦克的位置和底片
+    # Update position and images of player's tank
     player_tank.update_pos_img()
 
-    # 更新敌方坦克的位置和底片
+    # Update position and images of enemies' tanks
     for t in enemy_tanks:
         t.update_pos_img()
         if count % 20 == 0:
             enemy_bullets.append(t.create_bullet())
 
-    # 更新敌方子弹的位置和状态
+    # Update position and images of enemies' bullets
     for b in enemy_bullets:
         b.update_state()
         b.update_pos()
@@ -330,7 +322,7 @@ while running:
             b.state = USELESS
             player_tank.state = EXPLODE
 
-    # 更新玩家子弹的位置和状态
+    # Update position and state of player's bullets
     for b in player_bullets:
         b.update_state()
         b.update_pos()
@@ -339,12 +331,13 @@ while running:
                 b.state = USELESS
                 t.state = EXPLODE
 
-    # 删除无用的图形元素: 超出窗口的子弹和被击中的坦克
+    # Delete useless bullets that are out of window.
+    # Delete Tanks that finished exploding.
     enemy_bullets = delete_useless(enemy_bullets)
     enemy_tanks = delete_useless(enemy_tanks)
     player_bullets = delete_useless(player_bullets)
 
-    # 分数和生命数计算
+    # Calculation of hp and score
     score = (E_TANK_NUM - len(enemy_tanks))*10
 
     if player_tank.state == USELESS:
@@ -355,7 +348,7 @@ while running:
     can.itemconfig(lives_text, text='lives: '+str(player_lives))
     can.itemconfig(score_text, text='score: '+str(score))
 
-    # 判定游戏是否结束
+    # Game is end or not
     if player_tank.state == USELESS:
         can.create_text(WIN_WIDTH/2, WIN_HEIGHT/2,
                     text='YOU LOSE!\nscore:'+str(score),
